edbca270addbde6ec857d4d1efc37ce004f10e49
Sharanya128 <138455776+Sharanya128@users.noreply.github.com>
2023-07-30 18:34:03 +0530

    Merge sort.cpp

--- Merge sort.cpp
+++ Merge sort.cpp
@@ -3,72 +3,68 @@ using namespace std;
 
 void merge(int a[], int p, int q, int r)
 {
-      int n1 = q - p + 1;
-      int n2 = r - q;
-
-      int L[n1], M[n2];
-
-      for (int i = 0; i < n1; i++)
-        L[i] = a[p + i];
-      for (int j = 0; j < n2; j++)
-        M[j] = a[q + 1 + j];
-      int i, j, k;
-      i = 0;
-      j = 0;
-      k = p;
-      while (i < n1 && j < n2) {
-        if (L[i] <= M[j]) {
-          a[k] = L[i];
-          i++;
-        } else {
-          a[k] = M[j];
-          j++;
+    int n1 = q-p+1;
+    int n2 = r-q;
+    
+    int L[n1], M[n2];
+    
+    for(int i=0; i<n1; i++)
+        L[i] = a[p+i];
+    for(int j=0; j<n2; j++)
+        M[j]=a[q+1+j];
+        
+    int i=0, j=0, k=p;
+    
+    while(i<n1 && j<n2)
+    {
+        if(L[i]<=M[j])
+        {
+            a[k] = L[i];
+            i++;
+        }
+        else
+        {
+            a[k] = M[j];
+            j++;
         }
         k++;
-      }
-      while (i < n1) {
-        a[k] = L[i];
+    }
+    while(i<n1)
+    {
+        a[k]=L[i];
         i++;
         k++;
-      }
-      while (j < n2) {
-        a[k] = M[j];
+    }
+    while(j<n2)
+    {
+        a[k]=M[j];
         j++;
         k++;
-      }
     }
-    void mergeSort(int a[], int l, int r) \
+}
+void mergeSort(int a[], int l, int r)
+{
+    if(l<r)
     {
-      if (l < r)
-        {
-            int m = l + (r - l) / 2;
-
-            mergeSort(a, l, m);
-            mergeSort(a, m + 1, r);
-            merge(a, l, m, r);
-      }
+        int m=l+(r-l)/2;
+        mergeSort(a,l,m);
+        mergeSort(a,m+1,r);
+        merge(a,l,m,r);
     }
-
-    // Print the array
-    void printArray(int a[], int n) {
-      for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
-      cout << endl;
-    }
-
-    // Driver program
-    int main() 
+}
+int main()
+{
+    int a[100],n;
+    cout << "Enter no of ele:";
+    cin >> n;
+    cout << "Enter elements: ";
+    for(int i=0;i<n;i++)
     {
-      int a[100],n;
-      cout << "Enter number of elements: ";
-      cin >> n;
-      cout << "Enter elements of the array: ";
-      for (int i = 0; i<n; i++)
         cin >> a[i];
-
-      mergeSort(a, 0, n - 1);
-
-      cout << "Sorted array: \n";
-      printArray(a, n);
-      return 0;
     }
+    mergeSort(a,0,n-1);
+    cout<<"Sorted array:" ;
+    for(int i=0;i<n;i++)
+        cout << a[i] << " ";
+    return 0;
+}
